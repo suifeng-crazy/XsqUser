fec1a3a9d55985009278f018f57f7d5687e63ce3
suifeng-crazy <1047541809@qq.com>
2017-09-30 13:42:24 +0800

    sdf
    12

--- app/src/main/java/com/example/xsq/Login/BindPhoneActivity.java
+++ app/src/main/java/com/example/xsq/Login/BindPhoneActivity.java
@@ -1,7 +1,6 @@
 package com.example.xsq.Login;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
