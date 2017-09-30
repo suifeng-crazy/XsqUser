3aa8fc769a73ab97ee93953b24a4ec8ce9375f96
suifeng-crazy <32387637+suifeng-crazy@users.noreply.github.com>
2017-09-30 14:26:23 +0800

    Update Login.java

--- app/src/main/java/com/example/xsq/Login/Login.java
+++ app/src/main/java/com/example/xsq/Login/Login.java
@@ -14,7 +14,7 @@ import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
 import com.baidu.location.Poi;
 import com.example.R;
-
+// 注解 9.30 1426
 import java.util.List;
 public class Login extends AppCompatActivity {
 
