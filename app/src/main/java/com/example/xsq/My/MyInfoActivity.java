165157433e295673eb57d598ac2164f56d4f1c09
suifeng-crazy <1047541809@qq.com>
2017-10-20 10:34:12 +0800

    10.34 个人信息基本完成

--- app/src/main/java/com/example/xsq/My/MyInfoActivity.java
+++ app/src/main/java/com/example/xsq/My/MyInfoActivity.java
@@ -49,6 +49,8 @@ public class MyInfoActivity extends BaseActivity {
     private final static String URL_HEADER_UPLOAD = "http://192.168.1.191:8081/fileMg/up";
     private final static String URL_MODIFY_HEADER_IMG = "http://192.168.1.191/app/mg/inx/ucc/sbm";
 
+    String mmm;
+
     SelectPicPopupWindow menuWindow;
     RelativeLayout mReChangePhone, mReNickName, mReRealName, mReSex, mReHeaderImage;
     ImageView mImLeave, mImHeader;
