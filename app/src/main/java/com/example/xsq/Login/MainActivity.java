3db60e31c719e340efd712c66a9a8e2f91511d75
suifeng-crazy <1047541809@qq.com>
2017-10-20 10:03:32 +0800

    10.120 个人信息基本完成

--- app/src/main/java/com/example/xsq/Login/MainActivity.java
+++ app/src/main/java/com/example/xsq/Login/MainActivity.java
@@ -25,12 +25,12 @@ import static com.example.xsq.util.JsonStringMapUtil.checkUserPhone;
 
 // 直接在 git 上修改。看能否下载下来。
 public class MainActivity extends BaseActivity {
-    //  longinMath 是登录方法， 1代表用户名密码登录， 2为 短信验证码登录
+    //  mILonginMath 是登录方法， 1代表用户名密码登录， 2为 短信验证码登录
     // i 代表 执行哪个 runable ， 做区分用即可。
-    //  getCodeMath 代表 是因为 登录获取验证码  0，还是注册获取验证码  1。
-    int i = 0, resStatus = 3, longinMath = 1, getCodeMath = 0;
+    //  mIGetCodeMath 代表 是因为 登录获取验证码  0，还是注册获取验证码  1。
+    int mIi = 0, mIResStatus = 3, mILonginMath = 1, mIGetCodeMath = 0;
     // 获取验证码是否成功      注册验证码
-    public Boolean isGetTrue, isGetRegTrue;
+    public Boolean mBoIsGetTrue, mBoIsGetRegTrue;
     public Button mBtLogin;
     public EditText userNameE, passwordE, login_phoneCodeI;
     public String userName, password, phoneCode;
@@ -99,7 +99,7 @@ public class MainActivity extends BaseActivity {
                 break;
             case R.id.login_loginB:
                 if (mBtLogin.getText().toString().equals(getResources().getString(R.string.login))) {
-                    if (longinMath == 1) {
+                    if (mILonginMath == 1) {
                         login();
                     } else {
                         loginByPhone();
@@ -113,7 +113,7 @@ public class MainActivity extends BaseActivity {
                 }
                 break;
             case R.id.login_getCodeT: // 这里判断是 登录还是注册
-                if (getCodeMath == 1) {
+                if (mIGetCodeMath == 1) {
                     getCodeMeth();  // 读秒
                     getCode(2); // 获取短信验证码 注册用短信验证码
                 } else {
@@ -123,26 +123,26 @@ public class MainActivity extends BaseActivity {
                 break;
             case R.id.login_changeLoginMath:
                 if (login_passwordL.getVisibility() == View.VISIBLE) {
-                    i = 0;
+                    mIi = 0;
                     login_passwordL.setVisibility(View.INVISIBLE);
                     login_phoneCodeL.setVisibility(View.VISIBLE);
                     login_getCodeT.setVisibility(View.VISIBLE);
                     login_verLine.setVisibility(View.VISIBLE);
-                    longinMath = 2;
+                    mILonginMath = 2;
                     chang_longinMathT.setText(getResources().getString(R.string.login_zhmmdl));
                 } else {
-                    i = 1;
+                    mIi = 1;
                     login_passwordL.setVisibility(View.VISIBLE);
                     login_phoneCodeL.setVisibility(View.INVISIBLE);
                     login_getCodeT.setVisibility(View.INVISIBLE);
                     login_verLine.setVisibility(View.INVISIBLE);
-                    longinMath = 1;
+                    mILonginMath = 1;
                     chang_longinMathT.setText(getResources().getString(R.string.chang_longinMath));
                 }
                 break;
             case R.id.registerT: // 点击注册事件
                 login_forgetPasswrod.setVisibility(View.INVISIBLE);
-                getCodeMath = 1;
+                mIGetCodeMath = 1;
                 registerT.setBackgroundResource(R.drawable.base_text_bottom_hx_blue);
                 registerT.setTextColor(getResources().getColor(R.color.colorBlue));
                 login_loginT.setBackgroundResource(R.drawable.base_text_bottom_hx_null);
@@ -156,8 +156,8 @@ public class MainActivity extends BaseActivity {
 //                mBtLogin.setV
                 break;
             case R.id.login_loginT:
-                getCodeMath = 0;
-                longinMath = 1;
+                mIGetCodeMath = 0;
+                mILonginMath = 1;
                 login_forgetPasswrod.setVisibility(View.VISIBLE);
                 registerT.setBackgroundResource(R.drawable.base_text_bottom_hx_null);
                 login_loginT.setBackgroundResource(R.drawable.base_text_bottom_hx_blue);
@@ -179,7 +179,7 @@ public class MainActivity extends BaseActivity {
             toastMessage(MainActivity.this, "请输入验证码");
         } else {
             // 判断验证码是否正确。
-            i = 4;
+            mIi = 4;
             cachedThreadPool.execute(mRunnable);
         }
     }
@@ -195,8 +195,8 @@ public class MainActivity extends BaseActivity {
             userName = userNameE.getText().toString().trim();
             phoneCode = login_phoneCodeI.getText().toString().trim();
             if (!userName.equals("") && !phoneCode.equals("")) {
-                longinMath = 2;
-                i = 0;
+                mILonginMath = 2;
+                mIi = 0;
                 cachedThreadPool.execute(mRunnable);  // 这里没有对 mRunnable 进行标示 ， 都是跳转到 mRunnable
             } else {
                 hideDialog();
@@ -220,7 +220,7 @@ public class MainActivity extends BaseActivity {
                 if (password.length() >= 6) {
                     // 这个 cachedThreadPool 就是new Thread 开启的新的子线程， execute 就是线程的执行。
                     // mRunable 就是执行的  Runnable ， 之后就一样了。
-                    i = 0;
+                    mIi = 0;
                     cachedThreadPool.execute(mRunnable);  // 这里没有对 mRunnable 进行标示 ， 都是跳转到 mRunnable
                 } else {
                     hideDialog();
@@ -245,36 +245,36 @@ public class MainActivity extends BaseActivity {
         @Override
         public void run() {
             try {
-                if (i == 0) {
-                    if (longinMath == 1) {
-                        resStatus = PostParma.getToken("_a=" + userName, "_p=" + password);
+                if (mIi == 0) {
+                    if (mILonginMath == 1) {
+                        mIResStatus = PostParma.getToken("_a=" + userName, "_p=" + password);
                     } else {
-                        resStatus = PostParma.getToken("_a=" + userName, "_c=" + login_phoneCodeI.getText().toString());
+                        mIResStatus = PostParma.getToken("_a=" + userName, "_c=" + login_phoneCodeI.getText().toString());
                     }
-                    if(resStatus==0){
+                    if(mIResStatus==0){
                         mHandler.sendEmptyMessage(STATUS_ERROR);
                     }else {
                         mHandler.sendEmptyMessage(STATUS_CHECK);
                     }
 //                    mHandler.sendEmptyMessage(STATUS_CHECK);
-                } else if (i == 1) {
+                } else if (mIi == 1) {
                     // 获取短信验证码，登陆用
-                    isGetTrue = PostParma.getPhoneCode(ConnectionAddress.Base_Get_PhoneLoginCode, userName);
-                    if(!isGetTrue){
+                    mBoIsGetTrue = PostParma.getPhoneCode(ConnectionAddress.Base_Get_PhoneLoginCode, userName);
+                    if(!mBoIsGetTrue){
                         mHandler.sendEmptyMessage(STATUS_ERROR);
                     }else {
                         mHandler.sendEmptyMessage(888);
                     }
-                } else if (i == 2) {  // 获取短信验证码，注册用
-                    isGetRegTrue = PostParma.getPhoneCode(ConnectionAddress.Base_Get_PhoneCode, userName);
-                    if(!isGetRegTrue){
+                } else if (mIi == 2) {  // 获取短信验证码，注册用
+                    mBoIsGetRegTrue = PostParma.getPhoneCode(ConnectionAddress.Base_Get_PhoneCode, userName);
+                    if(!mBoIsGetRegTrue){
                         mHandler.sendEmptyMessage(STATUS_ERROR);
                     }else {
                         mHandler.sendEmptyMessage(999);
                     }
-                } else if (i == 4) {
-                    isGetRegTrue = PostParma.isTruePhoneCode(ConnectionAddress.Base_IsTrue_PhoneCode, userName, login_phoneCodeI.getText().toString());
-                    if(!isGetRegTrue){
+                } else if (mIi == 4) {
+                    mBoIsGetRegTrue = PostParma.isTruePhoneCode(ConnectionAddress.Base_IsTrue_PhoneCode, userName, login_phoneCodeI.getText().toString());
+                    if(!mBoIsGetRegTrue){
                         mHandler.sendEmptyMessage(STATUS_ERROR);
                     }else{
                         mHandler.sendEmptyMessage(STATUS_MORE);
@@ -297,12 +297,12 @@ public class MainActivity extends BaseActivity {
                     toastMessage(MainActivity.this, NumberUtil.strError);
                     break;
                 case 888:
-                    if (!isGetTrue) {
+                    if (!mBoIsGetTrue) {
                         toastMessage(MainActivity.this, NumberUtil.strError);
                     }
                     break;
                 case 999:
-                    if (!isGetRegTrue) {
+                    if (!mBoIsGetRegTrue) {
                         toastMessage(MainActivity.this, NumberUtil.strError);
                     }
                     break;
@@ -310,7 +310,7 @@ public class MainActivity extends BaseActivity {
                     hideDialog();
                     break;
                 case STATUS_CHECK: {  // 这个其实就是  query.getQueryStatus()
-                    if (resStatus == 1) {
+                    if (mIResStatus == 1) {
                         // 下面这是保存到自己的数据库中的。
                         SharedPreferences.Editor editor = getSharedPreferences("data", MODE_PRIVATE).edit();
                         editor.putString("userName", userName);
@@ -320,14 +320,14 @@ public class MainActivity extends BaseActivity {
                         NumberUtil.baseBottomSelectUI = 1;
                         strActivity(MainActivity.this, HomeActivity.class, true, false);
                         setGo(true);
-                    } else if (resStatus == 2) {
+                    } else if (mIResStatus == 2) {
                         hideDialog();
                         toastMessage(MainActivity.this, NumberUtil.strError);
                     }
                 }
                 break;
                 case STATUS_MORE: {
-                    if (isGetRegTrue) {
+                    if (mBoIsGetRegTrue) {
                         Intent intent = new Intent();
                         intent.setClass(MainActivity.this, LoginPasswordActivity.class);
                         intent.putExtra("userPhone", userName);
@@ -373,7 +373,7 @@ public class MainActivity extends BaseActivity {
                 if (checkUserPhone(userName)) {  // 这里应该是 判断手机号是否正确
                     // 这个 cachedThreadPool 就是new Thread 开启的新的子线程， execute 就是线程的执行。
                     // mRunable 就是执行的  Runnable ， 之后就一样了。
-                    i = j;
+                    mIi = j;
                     cachedThreadPool.execute(mRunnable);  // 这里没有对 mRunnable 进行标示 ， 都是跳转到 mRunnable
                 } else {
                     hideDialog();
